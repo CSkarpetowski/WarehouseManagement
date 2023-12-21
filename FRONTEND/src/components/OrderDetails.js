498e12cc817225006c50710b0eec282c5daba6f4
Przemysław Kossakowski <kossakowskiprzemek@gmail.com>
2023-12-21 23:34:13 +0100

    PrintOrder + PrintProduct

--- FRONTEND/src/components/OrderDetails.js
+++ FRONTEND/src/components/OrderDetails.js
@@ -1,71 +1,94 @@
 import React, { useEffect, useState } from 'react';
+import ReactToPrint from 'react-to-print';
 import './OrderDetails.css';
 import axios from 'axios';
 
-export default function OrderDetails(props) {
-    const {orderId, handleClose} = props;
-    const [orderDetails, setOrderDetails] = useState([]);
-    useEffect( () =>{
-      try{
-        axios.get(`https://localhost:7099/api/zamowienie/details/${orderId}`)
-        .then( (response) => {
-          const orderDataArray = [response.data];
-          setOrderDetails(orderDataArray[0]);
-          console.log(orderDataArray);
-        });
-      }
-      catch (err){
-          console.log(err);
-      }
-    },[orderId]);
+class ComponentToPrint extends React.Component {
+  render() {
+    const { orderDetails, orderId } = this.props;
 
-    const deleteOrder = () => {
-      try{
-        axios.delete(`https://localhost:7099/api/zamowienie/delete/${orderId}`)
-        .then(() => alert("Zamówienie usunięto"));
-      }
-      catch(err)
-      {
-        console.log(err);
-      }
-    };
-  return (
-    <div className='oderDetailsMain'>
-        <h1 className='orderWindowBar'>
-        <p onClick={handleClose} className='closeButton'>X</p>
-        Client ID :  {orderId}
-        </h1>
-        <div className='oderDetailsTableArea'>
-
-        <p style={{color:'white',
-         fontSize:'30px',
-          textAlign:'center',
-           fontFamily:'Roboto, Helvetica, sans-serif',
-           margin:'0'}} >
-            Oder details</p>
-
-        <table className='orderDetailsTable'>
+    return (
+      <div>
+        <h1 className='printClient'>Client ID : {orderId}</h1>
+        <table className="orderDetailsTable">
           <thead>
             <tr>
-            <th>Nazwa</th>
-            <th>Quantity</th>
-            <th>LOT</th>
-            <th>WarehouseID</th>
+              <th>Nazwa</th>
+              <th>Quantity</th>
+              <th>LOT</th>
+              <th>WarehouseID</th>
             </tr>
           </thead>
           <tbody>
-              { orderDetails.length > 0 && orderDetails.map((det) => (
-              <tr key={det.lpZamowienie}>
-                <td>{det.produkty.nazwa}</td>
-                <td>{det.ilosc}</td>
-                <td>{det.lot}</td>
-                <td>{det.produkty.pIdMagazyn}</td>
-              </tr>
-            ))}
-            </tbody>
+            {orderDetails.length > 0 &&
+              orderDetails.map((det) => (
+                <tr key={det.lpZamowienie}>
+                  <td>{det.produkty.nazwa}</td>
+                  <td>{det.ilosc}</td>
+                  <td>{det.lot}</td>
+                  <td>{det.produkty.pIdMagazyn}</td>
+                </tr>
+              ))}
+          </tbody>
         </table>
-        </div>
-        <button className='orderDetailsDelete' onClick={deleteOrder}>Delete Order</button>
+      </div>
+    );
+  }
+}
+
+export default function OrderDetails(props) {
+  const { orderId, handleClose } = props;
+  const [orderDetails, setOrderDetails] = useState([]);
+  const componentRef = React.createRef();
+
+  useEffect(() => {
+    try {
+      axios.get(`https://localhost:7099/api/zamowienie/details/${orderId}`).then((response) => {
+        const orderDataArray = [response.data];
+        setOrderDetails(orderDataArray[0]);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, [orderId]);
+
+  const deleteOrder = () => {
+    try {
+      axios.delete(`https://localhost:7099/api/zamowienie/delete/${orderId}`).then(() => alert('Zamówienie usunięto'));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="orderDetailsMain">
+      <h1 className="orderWindowBar">
+        <p onClick={handleClose} className="closeButton">
+          X
+        </p>
+        Client ID : {orderId}
+      </h1>
+      <div className="orderDetailsTableArea">
+        <p
+          style={{
+            color: 'white',
+            fontSize: '30px',
+            textAlign: 'center',
+            fontFamily: 'Roboto, Helvetica, sans-serif',
+            margin: '0',
+          }}
+        >
+          Order details
+        </p>
+        <ComponentToPrint orderDetails={orderDetails} orderId={orderId} ref={componentRef} />
+      </div>
+      <ReactToPrint
+        trigger={() => <button className="orderDetailsPrint">Print Order</button>}
+        content={() => componentRef.current}
+      />
+      <button className="orderDetailsDelete" onClick={deleteOrder}>
+        Delete Order
+      </button>
     </div>
-  )
+  );
 }
--- FRONTEND/src/components/WarehouseOne.js
+++ FRONTEND/src/components/WarehouseOne.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import './WarehousePage.css';
 import NavBar from './NavBar';
 import QRCode from 'qrcode.react';
+import { useReactToPrint } from 'react-to-print';
+import { PiPrinter } from "react-icons/pi";
 
 const WarehouseOne = () => {
   const [selectedRow, setSelectedRow] = useState(null);
   const [warehouseData, setWarehouseData] = useState([]);
+  const leftPanelRef = useRef();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,57 +25,52 @@ const WarehouseOne = () => {
     fetchData();
   }, []);
 
-
-  const alertValidationOK = (event) =>{
+  const alertValidationOK = (event) => {
     let button = event.target;
     let buttonID = button.id;
-     try{
-       axios.patch(`https://localhost:7099/api/produkt/isGood/${buttonID}`, {isGood:true})
-       .then(() => {
-         console.log("Pomyslnie zaktualizowano isGood TRUE ");
-         window.location.reload();
-       })
-        }
-     catch (err){
-         console.log(err);
-     } 
-}
+    try {
+      axios.patch(`https://localhost:7099/api/produkt/isGood/${buttonID}`, { isGood: true })
+        .then(() => {
+          console.log("Pomyslnie zaktualizowano isGood TRUE ");
+          window.location.reload();
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  const alertValidationNOK = (event) =>{
+  const alertValidationNOK = (event) => {
     let button = event.target;
     let buttonID = button.id;
-     try{
-       axios.patch(`https://localhost:7099/api/produkt/isGood/${buttonID}`, {isGood:false})
-       .then(() => {
-         console.log("Pomyslnie zaktualizowano isGood FALSE ");
-         window.location.reload();
-       })
-        }
-     catch (err){
-         console.log(err);
-     } 
-  }
+    try {
+      axios.patch(`https://localhost:7099/api/produkt/isGood/${buttonID}`, { isGood: false })
+        .then(() => {
+          console.log("Pomyslnie zaktualizowano isGood FALSE ");
+          window.location.reload();
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-  
-  const LeftPanel = ({ selectedRow }) => {
+  const LeftPanel = React.forwardRef(({ selectedRow }, ref) => {
     const [qrCodeValue, setQRCodeValue] = useState('');
-  
+
     useEffect(() => {
       if (selectedRow) {
         const { nazwa, lot, ilosc, pIdMagazyn } = selectedRow;
         setQRCodeValue(JSON.stringify({ nazwa, lot, ilosc, pIdMagazyn }));
       }
     }, [selectedRow]);
-  
-    // Sprawdzenie czy nie jest null
+
     const decodedInfo = qrCodeValue ? JSON.parse(qrCodeValue) : null;
-  
+
     return (
-      <div className="warehouseleftPanel">
+      <div ref={ref} className="warehouseleftPanel">
+        <PiPrinter color='#c87cfc' className='printButton'onClick={handlePrintLeftPanel}/>
         {qrCodeValue ? (
           <QRCode className='qrCode' value={qrCodeValue} />
         ) : (
-          // Jesli null to bialy kwadrat w jego miejsce
           <div className='qrCode' style={{ width: '128px', height: '128px', backgroundColor: 'white' }}>
           </div>
         )}
@@ -84,51 +82,55 @@ const WarehouseOne = () => {
         )}
       </div>
     );
+  });
+
+  const RightPanel = ({ warehouseData, setSelectedRow }) => {
+    return (
+      <div className="warehouserightPanel">
+        <table className='qrTable'>
+          <thead>
+            <tr style={{ textAlign: 'center' }}>
+              <th>ID Prod</th>
+              <th>Nazwa</th>
+              <th>LOT</th>
+              <th className="itemQuantity">Ilość palet</th>
+              <th>Alert</th>
+            </tr>
+          </thead>
+          <tbody className='warehousetable'>
+            {warehouseData.map((val) => (
+              <tr key={val.idProd}
+                style={{ backgroundColor: val.isGood ? 'white' : 'red', }}
+                onClick={() => setSelectedRow(val)}>
+                <td>{val.idProd}</td>
+                <td>{val.nazwa}</td>
+                <td>{val.lot}</td>
+                <td>{val.ilosc}</td>
+                <td>
+                  <button style={{ width: '25%', backgroundColor: 'green', margin: '2px' }} id={val.idProd} onClick={alertValidationOK}></button>
+                  <button style={{ width: '25%', backgroundColor: 'tomato', margin: '2px' }} id={val.idProd} onClick={alertValidationNOK}></button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
   };
 
-const RightPanel = ({ warehouseData, setSelectedRow }) => {
-  return (
-    <div className="warehouserightPanel">
-      <table className='qrTable'>
-        <thead>
-          <tr style={{ textAlign: 'center' }}>
-            <th>ID Prod</th>
-            <th>Nazwa</th>
-            <th>LOT</th>
-            <th className="itemQuantity">Ilość palet</th>
-            <th>Alert</th>
-          </tr>
-        </thead>
-        <tbody className='warehousetable'>
-          {warehouseData.map((val) => (
-            <tr key={val.idProd} 
-              style={{backgroundColor: val.isGood ? 'white' : 'red',}} 
-              onClick={() => setSelectedRow(val)}>
-              <td>{val.idProd}</td>
-              <td>{val.nazwa}</td>
-              <td>{val.lot}</td>
-              <td>{val.ilosc}</td>
-              <td>
-                <button style={{ width: '25%', backgroundColor: 'green', margin: '2px' }} id={val.idProd} onClick={alertValidationOK}></button>
-                <button style={{ width: '25%', backgroundColor: 'tomato', margin: '2px' }} id={val.idProd} onClick={alertValidationNOK}></button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+  const handlePrintLeftPanel = useReactToPrint({
+    content: () => leftPanelRef.current,
+  });
 
   return (
     <div id='WarehousePage'>
       <NavBar />
       <h1 className='cardTitle'>- Warehouse 1 -</h1>
       <div className='warehousePageArea'>
-      <div className="warehousetablesContainer">
-        <LeftPanel selectedRow={selectedRow} />
-        <RightPanel warehouseData={warehouseData} setSelectedRow={setSelectedRow} />
-      </div>
+        <div className="warehousetablesContainer">
+          <LeftPanel selectedRow={selectedRow} ref={leftPanelRef} />
+          <RightPanel warehouseData={warehouseData} setSelectedRow={setSelectedRow} />
+        </div>
       </div>
     </div>
   );
